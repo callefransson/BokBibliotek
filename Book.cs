600fdb5656f36734eba8dbb1ca5b85037022d718
calle fransson <calle-fransson@hotmail.com>
2023-10-05 11:54:11 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BokBibliotek
+{
+    internal class Book
+    {
+        //egenskaperna som används i programmet
+
+        public string _title;
+        public string _author;
+        public string _summary;
+        public Book(string title, string auther, string summary) //Konstruktorn som initierar värderna på variablerna
+        {
+            _title = title;
+            _author = auther;
+            _summary = summary;
+        }
+
+        public void PrintBook() // Metoden för att skriva ut värderna i variablerna
+        {
+            Console.WriteLine("The title of the book : {0}\nAuther: {1}\nSummary of the book: {2}",_title,_author,_summary);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace BokBibliotek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("title","auther", "summary"); 
+
+            Console.WriteLine("Please write the title of the book");
+            book._title = Console.ReadLine();
+            Console.WriteLine("Please write in the auther of the book");
+            book._author = Console.ReadLine();
+            Console.WriteLine("Please write in the summary of the book");
+            book._summary = Console.ReadLine();
+
+            book.PrintBook();// Anropar metoden för att se vad användaren skrev 
+        }
+    }
+}
